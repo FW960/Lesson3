31b6bb254821dabbe2900b203b1101a37e7b1a7a
FW960 <artuninp@gmail.com>
2022-02-05 02:07:26 +0800

    МорскойБой

--- PeterHM/PeterHM/Program.cs
+++ PeterHM/PeterHM/Program.cs
@@ -6,7 +6,50 @@ namespace PeterHM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[,] b = new string[10, 10];
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    b[i, j] = "O";
+
+                    
+                }
+                   
+            }
+            Console.WriteLine("Морской бой");
+            Console.WriteLine();
+            b[1, 1] = "X";
+            b[2, 1] = "X";
+            b[2, 4] = "X";
+            b[2, 7] = "X";
+            b[2, 8] = "X";
+            b[2, 9] = "X";
+            b[3, 3] = "X";
+            b[3, 4] = "X";
+            b[3, 5] = "X";
+            b[3, 8] = "X";
+            b[4, 9] = "X";
+            b[5, 5] = "X";
+            b[6, 2] = "X";
+            b[6, 3] = "X";
+            b[6, 5] = "X";
+            b[7, 5] = "X";
+            b[7, 7] = "X";
+            b[8, 1] = "X";
+            b[8, 5] = "X";
+            b[9, 8] = "X";
+
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    Console.Write($"{b[i, j]} ");
+                }
+                Console.WriteLine();
+            } Console.WriteLine();
+
+
         }
     }
 }
