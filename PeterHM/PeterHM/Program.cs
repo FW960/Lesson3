c04ea952f5892f372af50dc2f409ce7932d905bb
FW960 <artuninp@gmail.com>
2022-02-05 01:43:45 +0800

    ТекстПоДиагонали

--- PeterHM/PeterHM/Program.cs
+++ PeterHM/PeterHM/Program.cs
@@ -6,7 +6,18 @@ namespace PeterHM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Текст по диагонали");
+
+            string[,] b = new string[4, 2] { { "1", " " }, { " ", "1" }, { "1", " " }, { " ", "1" } };
+
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    Console.Write($"{b[i, j]}");
+                }
+                Console.WriteLine();
+            }   Console.WriteLine();
         }
     }
 }
